c4cdb45f91c73a0f1a4db15f69b87b1a96268972
Miles Wu <miles.wu@wu-home.co.uk>
2010-05-13 08:10:31 +0100

    Geoip

---
+++ leaves/geoip/controller.rb
@@ -0,0 +1,26 @@
+# Controller for the Geoip leaf.
+require 'geoip'
+require 'resolv'
+
+class Controller < Autumn::Leaf
+	@@geo = GeoIP.new("GeoLiteCity.dat")
+	
+	def geoip_command(stem, sender, reply_to, msg)
+		begin
+			r = @@geo.city(msg)
+			ip = r[1].split(".")
+			
+			origin = Resolv::DNS.new.getresource("#{ip[3]}.#{ip[2]}.#{ip[1]}.#{ip[0]}.origin.asn.cymru.com", Resolv::DNS::Resource::IN::TXT).strings.first
+			asn = origin.split("|")[0].strip rescue ""
+			unless asn == ""
+				asn2 = Resolv::DNS.new.getresource("as#{asn}.asn.cymru.com", Resolv::DNS::Resource::IN::TXT).strings.first
+				puts asn2
+				isp = "-- " + asn2.split("|")[4].lstrip.strip
+			end
+
+			return "#{r[1]}: #{r[7]}, #{r[6]}, #{r[4]} #{isp}"
+		rescue
+			return "Error - sowwy"
+		end
+	end
+end
